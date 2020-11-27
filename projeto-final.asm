e4048afae94764253894f77c586b65a13b7393b5
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2020-11-27 23:59:15 +0000

    Added keyboard support, added comments

--- projeto-final.asm
+++ projeto-final.asm
@@ -44,6 +44,9 @@ TECLA_COMECAR EQU 0CH
 TECLA_PAUSAR EQU 0DH
 TECLA_TERMINAR EQU 0EH
 
+TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
+TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
+
 IMAGEM_PAUSA EQU 0
 
 ; *********************************************************************************
@@ -110,9 +113,9 @@ func_nave:
 ; Variaveis de estado
 ; *********************************************************************************
 
-jogo: WORD 1           ; estado do jogo (-1 = por começar, 0 = pausa , 1 = a correr)
+jogo: WORD -1           ; estado do jogo (-1 = por começar, 0 = pausa , 1 = a correr)
 
-tecla_premida: WORD 0H   ; tecla premida (-1 = nao houve tecla, (0-F) = tecla do teclado )
+tecla_premida: WORD -1   ; tecla premida (-1 = nao houve tecla, (0-F) = tecla do teclado )
 ultima_tecla: word -1 ; ultima tecla premida (-1 = nao houve tecla, (0-F) = tecla do teclado )
 
 energia: word ENERGIA_INICIAL  ; energia da nave
@@ -159,7 +162,65 @@ ciclo:
 
 
 teclado:
-  RET
+  PUSH R0				; Salva registos
+  PUSH R1
+  PUSH R2
+  PUSH R3
+  PUSH R5
+  PUSH R6
+
+  MOV R0, ultima_tecla			; Obt�m endere�o da mem�ria onde a  ultima tecla est� guardada
+  MOV R1, tecla_premida ; Obt�m endere�o da mem�ria onde a  ultima tecla est� guardada
+  MOV R1, [R1] ; Obtem o valor da variavel
+  MOV [R0], R1 ; escreve na variavel ultima_tecla o valor atual da variavel tecla_premida
+
+  MOV  R2, TEC_LIN    ; Obt�m endere�o das linhas (periferico)
+  MOV  R3, TEC_COL    ; Obt�m endere�o das colunas (periferico)
+  MOV  R1, 8			; Linha onde come�a a verificar o teclado
+
+  loop_teclado:          ; Neste ciclo verificam-se todas as linhas do teclado
+    MOVB [R2], R1      ; Escreve a linha a verificar no perif�rico
+    MOVB R0, [R3]      ; L� do perif�rico a linha da tecla pressionada
+    CMP  R0, 0         ; Verifica se h� tecla premida
+    JNZ  clique        ; Se uma tecla estiver premida salta
+    SHR R1, 1		   ; Calcula a pr�xima linha
+    JZ sem_tecla	   ; Se j� verificou todas as linhas sai
+    JMP loop_teclado   ; Salta para manter o loop
+
+  clique:
+    MOV R5, -1          ; Guarda -1 em R5 para contagem de divisoes
+    conv_linha:         ; Converte linha(1,2,4,8) para (0,1,2,3) para calculos futuros
+      SHR R1, 1       ; Por cada shift
+      ADD R5, 1       ; Adiciona 1 para ficar 0-based
+      CMP R1, 0       ; Verifica se j� chegou ao fim do loop
+      JNZ conv_linha  ; Caso contr�rio continua at� R1 ser 0
+
+    MOV R6, -1			; Guarda -1 em R6 para contagem de divisoes
+    conv_coluna:	    ; Converte coluna(1,2,4,8) para (0,1,2,3) para calculos futuros
+      SHR R0, 1		; Por cada shift
+      ADD R6, 1       ; Adiciona 1 para ficar 0-based
+      CMP R0, 0		; Verifica se j� chegou ao fim do loop
+      JNZ conv_coluna ; Caso contr�rio continua at� R0 ser 0
+
+    SHL R5,2   			; Calculo da formula 4*linha + coluna para obter tecla
+    ADD R5, R6
+    JMP fim_verificar_teclado
+
+    sem_tecla:			; Se n�o houver tecla premida escreve -1 no registo que guarda a tecla
+      MOV R5, -1
+
+  fim_verificar_teclado:
+    MOV R0, tecla_premida			; Obt�m endere�o da mem�ria onde a tecla est� guardada
+    MOV [R0], R5			; Guarda nova tecla na vari�vel global tecla_premida
+
+  POP R6	; Repoe registos usados
+  POP R5
+  POP R3
+  POP R2
+  POP R1
+  POP R0
+  RET		; Retorna
+
 
 
 nave:
@@ -167,32 +228,35 @@ nave:
   PUSH R2
   PUSH R3
 
-
   MOV R1, jogo        ; base da tabela jogo
   MOV R1, [R1]        ; le o estado do jogo
   CMP R1, 1           ; se o jogo nao estiver a correr saimos
   JNZ sair_nave
+
 nave_energia:
   MOV R1, evento_int
   MOV R1, [R1]
-  CMP R1, 1  ; verificar se aconteceu a interropeçao responsavel pela energia
-  JNZ nave_ler_tecla ; se nao, saimos
-  CALL diminuir_energia
+  CMP R1, 1  ; verificar se aconteceu a interropeçao responsavel pela diminuição da energia
+  JNZ nave_ler_tecla ; se nao, continuamos
+  CALL diminuir_energia ; se sim, diminuimos a energia e só depois continuamos
+
 nave_ler_tecla:
   MOV R2, tecla_premida
   MOV R2, [R2]      ; verifica a tecla premida
-  CMP R2, 0         ; se a tecla pressionada for a tecla 0 entao movimentamos a nave para a esquerda
+  CMP R2, 0         ; se a tecla pressionada for menor que 0 significa que nenhuma tecla foi pressionada
+  JLT sair_nave    ; logo podemos sair da rotina e nao temos que movimentar a nave
   SHL R2, 1         ; multiplicar R2 por 2 porque a func_nave é uma tabela de words
   MOV R3, func_nave ; base da tabela das funcionalidades da nave
   MOV R3, [R3 + R2] ; obter a funcionalidade da tecla premida
 
-  CMP R3, NAO_MOVE ; se a tecla nao for relevante entao saimos
+  CMP R3, NAO_MOVE ; se a tecla nao for relevante entao saimos da rotina
   JZ sair_nave
-  CMP R3, DISPARA_MISSIL ; se a tecla dor a de disparar o missil procedemos como tal
+  CMP R3, DISPARA_MISSIL ; se a tecla for a de disparar o missil, procedemos como tal
   JZ nave_dispara_missil
-  ; caso a tecla nao seja nenhuma das anterioes então movimentamos a nave
+
+  ; caso a tecla premida nao seja nenhuma das anterioes então movimentamos a nave
   CALL movimenta_nave ; movimenta a nave
-  CALL desenha_nave
+  CALL desenha_nave ; desenha a nave
   JMP sair_nave
 nave_dispara_missil:
 
@@ -213,14 +277,12 @@ controlo:
 
   MOV R1, jogo        ; base da tabela jogo
   MOV R1, [R1]        ; le o estado do jogo
-  MOV R2, tecla_premida
-  MOV R2, [R2]      ; verifica a tecla premida
-  CMP R1, R2        ; comparar o estado do jogo com a tecla premida
-  JZ sair_controlo ; se for igual saimos
+  MOV R2, tecla_premida ; base da tabela da tecla premida
+  MOV R2, [R2]      ; le a tecla premida
 
   MOV R4, ultima_tecla ; base da tabela 'ultima tecla'
   MOV R4, [R4]  ; le o valor da variavel
-  CMP R2, R4 ; se o valor das variaveis for igual significa que nao deixamos de pressionar a teclas
+  CMP R2, R4 ; se o valor das variaveis for igual significa que nao deixamos de pressionar a tecla
   JZ sair_controlo ; nesse caso saimos
 
   MOV R5, TECLA_COMECAR
@@ -235,7 +297,7 @@ controlo:
   JMP sair_controlo
 
 comecar:
-  MOV R3, JOGO_CORRER
+  MOV R3, JOGO_CORRER ;
   CALL inicializar    ; inizialiar tudo com o seu valor original
   CALL tocar_video    ; toca o video
   JMP modificar_estado
@@ -274,6 +336,27 @@ sair_controlo:
 
 
 
+
+
+desenha_nave:
+  PUSH R1
+  PUSH R3
+  PUSH R4
+  PUSH R5
+  PUSH R11
+  MOV R5, nave_coordenadas
+  MOV R3, [R5]          ; coordenada X
+  MOV R4, [R5 + 2]         ; coordenada Y
+  MOV R1, nave_pixeis
+  MOV R11, LARGURA_NAVE        ; largura da nave
+  CALL desenha_objeto ; desenhar objeto
+  POP R11
+  POP R5
+  POP R4
+  POP R3
+  POP R1
+
+
 ; **********************************************************************
 ; INICIALIZAR - INICIALIZA O JOGO
 ; **********************************************************************
@@ -283,6 +366,7 @@ inicializar:
   MOV R1, energia  ; base da tabela da energia
   MOV R2, ENERGIA_INICIAL ; inicializa r2 com a energia inicial
   MOV [R1], R2 ; guarda o valor na memoria
+  CALL apagar_imagem
   POP R2
   POP R1
   RET
@@ -298,7 +382,7 @@ alterar_estado_jogo:
   PUSH R1
   PUSH R2
   MOV R1, jogo        ; base da tabela jogo
-  MOV [R1], R3     ; guarda
+  MOV [R1], R3     ; guarda o novo valor na variavel de estado
   POP R2
   POP R1
   RET
@@ -363,8 +447,14 @@ apagar_imagem:
   POP R0
   RET
 
+
+
+
 ; **********************************************************************
 ; MOVIMENTA_NAVE - movimenta a nave de acordo com a tecla pressionada
+;
+; Argumento: R3 - Velocidade da nave
+;
 ; **********************************************************************
 movimenta_nave:
   PUSH R2
@@ -372,20 +462,33 @@ movimenta_nave:
   PUSH R6
   PUSH R7
 
+verifica_limites:
   MOV R7, COL_MAX       ; limite direito do ecra (coluna 63)
   SUB R7, LARGURA_NAVE           ; subtrair a largura da nave
   MOV R5, nave_coordenadas  ; base da tabela das coordenadas
   MOV R6, [R5 + 2]      ; obter a coluna onde se situa a nave
   CMP R6, R7         ; comparar com a coluna máxima
-  JGT sair_movimenta_nave ;limitar o movimento da nave no lado direito do ecra
+  JGT pode_virar_esquerda ; se chegamos à coluna maxima, a unica opçao é andar para a esquerda
 
   MOV R7, COL_MIN
   CMP R6, R7      ; comparar com a coluna minima
-  JLE sair_movimenta_nave ;l imitar o movimento da nave no lado esquerdo do ecra
-  ADD R6, R3   ; adicionar a velocidade à posicao atual
-  JMP altera_posicao
+  JLE pode_virar_direita ; se chegamos à coluna minima, a unica opçao é andar para a direita
+
+  JMP altera_posicao ; se nao estivermos a tocar nos limites do ecra entao alteramos a posicao da nave
+
+pode_virar_esquerda:
+  CMP R3, 0 ; verificar se a velocidade é menor que 0, nesse caso estamos a tentar andar para a esquerda
+  JLT altera_posicao ; se sim, alteramos a posicao
+  JMP sair_movimenta_nave ; caso contrario saimos da rotina e nao movimentamos a nave
+
+pode_virar_direita:
+  CMP R3, 0 ; verificar se a velocidade é maior que 0, nesse caso estamos a tentar andar para a direita
+  JGT altera_posicao ; se sim, alteramos a posicao
+  JMP sair_movimenta_nave ; caso contrario saimos da rotina e nao movimentamos a nave
 
 altera_posicao:
+  ; caso geral
+  ADD R6, R3   ; adicionar a velocidade à posicao atual
   MOV [R5 + 2], R6  ; escreve a nova posicao na memoria
   CALL apagar_ecra  ;apaga o ecra para dar lugar ao objeto desenhado na nova posicao
 sair_movimenta_nave:
@@ -401,23 +504,7 @@ sair_movimenta_nave:
 
 
 
-desenha_nave:
-  PUSH R1
-  PUSH R3
-  PUSH R4
-  PUSH R5
-  PUSH R11
-  MOV R5, nave_coordenadas
-  MOV R3, [R5]          ; coordenada X
-  MOV R4, [R5 + 2]         ; coordenada Y
-  MOV R1, nave_pixeis
-  MOV R11, LARGURA_NAVE        ; largura da nave
-  CALL desenha_objeto ; desenhar objeto
-  POP R11
-  POP R5
-  POP R4
-  POP R3
-  POP R1
+
 
 
 
@@ -524,17 +611,17 @@ desenha:
 
 
 diminuir_energia:
-PUSH R1
-PUSH R2
+  PUSH R1
+  PUSH R2
 
-MOV R1, energia  ; base da tabela da energia
-MOV R2, [R1] ; le o valor de energia
-SUB R2, ENERGIA ; diminui a energia
-MOV [R1], R2 ; guarda o novo valor na memoria
+  MOV R1, energia  ; base da tabela da energia
+  MOV R2, [R1] ; le o valor de energia
+  SUB R2, ENERGIA ; diminui a energia
+  MOV [R1], R2 ; guarda o novo valor na memoria
 
-POP R2
-POP R1
-RET
+  POP R2
+  POP R1
+  RET
 
 
 
